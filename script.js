a3fcb98798ea6c66f5daa0e5dbb073c335840c85
AndreaDeGiorgi00 <andrea.adedegiorgi@gmail.com>
2023-01-11 17:04:28 +0100

    aggiunte le tasks in pagina e aggiunta la "x" per rimuoere una task

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log("vue ok", Vue)
+tasks = [{
+    text : "comprare le uova",
+    done : "true"
+},
+{
+    text : "andare dal meccanico",
+    done : "false"
+},
+{
+    text : "vendere la casa",
+    done : "true"
+}]
+
+
+const app = Vue.createApp({
+    data(){
+        return{
+            tasks , 
+            cuore : 1,
+        }
+    },
+    methods:{
+        hasDone(n){
+            if(this.tasks[n].done == "true"){
+                //è stata completata
+                return "checked"
+            }else{
+                return " todo"
+            }
+        },
+        removeTask(i){
+            this.tasks.splice(i,1)
+        }
+    }
+})
+
+
+
+
+
+app.mount(".contenitore")
